ae9633b9ec508cd6e53000c10120583f48adcc40
ARUN YADAV <arun3.yadav@aricent.com>
2020-02-11 17:28:18 +0530

    Initial commit- Adding basic code

---
+++ app/src/main/java/com/arunyadav/trendingdev/Dashboard.java
@@ -0,0 +1,13 @@
+package com.arunyadav.trendingdev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
